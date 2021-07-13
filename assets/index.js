c0a41d176731d74d1bc404779d583ce372c92205
NickyEXE <dovern42@gmail.com>
2021-07-12 21:32:53 -0400

    OO refactor, remove radio buttons, add reset ability

--- assets/index.js
+++ assets/index.js
@@ -1,27 +1,65 @@
 class Form {
   constructor(){
-    this.element = document.querySelector("form")
-    this.element.addEventListener("change", this.handleChange)
-    this.state = {
-      bill: 0.00,
-      tip: 0,
-      numberOfPeople: 0
-    }
+    this.form = document.querySelector("form")
+    this.form.addEventListener("change", this.handleChange)
+    this.state = this.constructor.initialState()
+    this.populateButtons()
+    this.addEventListeners()
+  }
+
+  setState = (key, value) => {
+    this.state[key] = value
+    console.log(this.state)
+    this.updateResults()
+  }
+
+  populateButtons = () => {
+    const radioDiv = document.querySelector(".radio")
+    radioDiv.innerHTML = ""
+    const array = [5, 10, 15, 25, 50]
+    array.forEach(int => {
+      radioDiv.innerHTML += `
+      <input type="button" class="radio-btn ${this.state.tip == int ? "selected" : ""}" data-tip="${int}" value="${int}%"/>
+      `
+    })
   }
 
   handleChange = (e) => {
-    console.log(e.target)
-    this.state[e.target.name] = parseFloat(e.target.value)
-    e.target.name == "numberOfPeople" && this.updateResults()
+    this.setState(e.target.name, parseFloat(e.target.value))
+  }
+
+  addEventListeners = () => {
+    document.querySelector(".radio").addEventListener("click", (e) => {
+      const btn = e.target.closest(".radio-btn")
+      if (btn) {
+        this.setState("tip", parseInt(btn.dataset.tip))
+        this.populateButtons()
+      }
+    })
+    document.getElementById("reset").addEventListener("click", this.reset)
+  }
+
+  resetResults = () => {
+    document.getElementById("tip-per-person").innerText = "$0.00"
+    document.getElementById("total-per-person").innerText = "$0.00"
+  }
+
+  reset = () => {
+    this.resetResults()
+    this.form.reset()
+    this.state = this.constructor.initialState()
+    console.log(this.state)
+    this.populateButtons()
   }
 
   updateResults = () => {
     const { tip, bill, numberOfPeople } = this.state
-    const tipPerPerson = (tip/100) * bill / numberOfPeople
-    const totalPerPerson = (1 + (tip/100)) * bill / numberOfPeople
-    console.log(tipPerPerson, totalPerPerson)
-    document.getElementById("tip-per-person").innerText = Form.toCurrency(tipPerPerson)
-    document.getElementById("total-per-person").innerText = Form.toCurrency(totalPerPerson)
+    if (tip * bill * numberOfPeople !== 0) {
+      const tipPerPerson = (tip/100) * bill / numberOfPeople
+      const totalPerPerson = (1 + (tip/100)) * bill / numberOfPeople
+      document.getElementById("tip-per-person").innerText = Form.toCurrency(tipPerPerson)
+      document.getElementById("total-per-person").innerText = Form.toCurrency(totalPerPerson)
+    }
   }
 
   static toCurrency = (float) => {
@@ -31,6 +69,13 @@ class Form {
     })
     return formatter.format(float)
   }
+
+  static initialState = () => ({
+    bill: 0.00,
+    tip: 0,
+    numberOfPeople: 0,
+    toggleCustom: false
+  })
 }
 
 new Form()
